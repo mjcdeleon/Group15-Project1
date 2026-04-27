6739a4eb0e9d443467afe213a920e502ad3fc394
Joyce Yang <joygraceyang@gmail.com>
2026-04-26 23:26:41 -0700

    added line renderer

--- Assets/Teleport.cs
+++ Assets/Teleport.cs
@@ -5,17 +5,51 @@ public class Teleporter : MonoBehaviour
 {
     public Transform xrOrigin;
     public LayerMask groundLayer;
-    public InputActionReference teleportAction; // Use this slot in Inspector!
+    public InputActionReference teleportAction;
 
-    void OnEnable() => teleportAction.action.Enable();
-    void OnDisable() => teleportAction.action.Disable();
+    private LineRenderer lineRenderer;
+
+    void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.enabled = false; // Keep it hidden until the user aims
+    }
 
     void Update()
     {
-        // Now it checks the specific action you drag into the Inspector
+        bool isHolding = teleportAction.action.ReadValue<float>() > 0.5f;
+
+        if (isHolding)
+        {
+            lineRenderer.enabled = true;
+            DrawTeleportRay();
+        }
+        else
+        {
+            lineRenderer.enabled = false;
+        }
+
         if (teleportAction.action.WasReleasedThisFrame())
         {
             TryTeleport();
+        }
+    }
+
+    void DrawTeleportRay()
+    {
+        RaycastHit hit;
+        // Start the line at the controller
+        lineRenderer.SetPosition(0, transform.position);
+
+        // If it hits the ground, draw to the hit point
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 10f, groundLayer))
+        {
+            lineRenderer.SetPosition(1, hit.point);
+        }
+        else
+        {
+            // Otherwise, draw a line into the air to show where we are aiming
+            lineRenderer.SetPosition(1, transform.position + (transform.forward * 10f));
         }
     }
 
@@ -25,10 +59,6 @@ public class Teleporter : MonoBehaviour
         if (Physics.Raycast(transform.position, transform.forward, out hit, 10f, groundLayer))
         {
             xrOrigin.position = hit.point;
-        }
-        else
-        {
-            Debug.Log("Teleport Raycast missed! Check your Ground Layer.");
         }
     }
 }
