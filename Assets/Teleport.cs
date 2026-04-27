601bc96efba52b57dc75edc5f427fbc38eeb713f
Joyce Yang <joygraceyang@gmail.com>
2026-04-27 00:05:15 -0700

    changed teleportation

--- Assets/Teleport.cs
+++ Assets/Teleport.cs
@@ -4,15 +4,25 @@ using UnityEngine.InputSystem;
 public class Teleporter : MonoBehaviour
 {
     public Transform xrOrigin;
-    public LayerMask groundLayer;
     public InputActionReference teleportAction;
 
+    public LayerMask groundLayer;
+    public float arcSpeed = 8f;
+    public float arcTimeStep = 0.05f;
+    public int arcResolution = 30;
+
+    public Color validColor = Color.green;
+    public Color invalidColor = Color.red;
+
     private LineRenderer lineRenderer;
+    private Vector3 validHitPoint;
+    private bool hasValidTarget;
 
     void Awake()
     {
         lineRenderer = GetComponent<LineRenderer>();
-        lineRenderer.enabled = false; // Keep it hidden until the user aims
+        lineRenderer.enabled = false;
+        lineRenderer.positionCount = arcResolution;
     }
 
     void Update()
@@ -22,43 +32,69 @@ public class Teleporter : MonoBehaviour
         if (isHolding)
         {
             lineRenderer.enabled = true;
-            DrawTeleportRay();
+            DrawArc();
         }
         else
         {
             lineRenderer.enabled = false;
+            hasValidTarget = false;
         }
 
-        if (teleportAction.action.WasReleasedThisFrame())
+        if (teleportAction.action.WasReleasedThisFrame() && hasValidTarget)
         {
             TryTeleport();
         }
     }
 
-    void DrawTeleportRay()
+    void DrawArc()
     {
-        RaycastHit hit;
-        // Start the line at the controller
-        lineRenderer.SetPosition(0, transform.position);
+        Vector3 pos = transform.position;
+        Vector3 velocity = transform.forward * arcSpeed;
+        hasValidTarget = false;
+        int validPointCount = arcResolution;
 
-        // If it hits the ground, draw to the hit point
-        if (Physics.Raycast(transform.position, transform.forward, out hit, 10f, groundLayer))
+        for (int i = 0; i < arcResolution; i++)
         {
-            lineRenderer.SetPosition(1, hit.point);
+            lineRenderer.SetPosition(i, pos);
+
+            Vector3 nextVelocity = velocity + Physics.gravity * arcTimeStep;
+            Vector3 nextPos = pos + velocity * arcTimeStep;
+
+            // Check for ground collision along this arc segment
+            if (Physics.Raycast(pos, velocity.normalized, out RaycastHit hit,
+                (nextPos - pos).magnitude, groundLayer))
+            {
+                validHitPoint = hit.point;
+                hasValidTarget = true;
+                validPointCount = i + 1;
+
+                // Fill remaining line positions at the hit point
+                for (int j = i + 1; j < arcResolution; j++)
+                    lineRenderer.SetPosition(j, validHitPoint);
+
+                break;
+            }
+
+            pos = nextPos;
+            velocity = nextVelocity;
         }
-        else
-        {
-            // Otherwise, draw a line into the air to show where we are aiming
-            lineRenderer.SetPosition(1, transform.position + (transform.forward * 10f));
-        }
+
+        // Color the line green if valid, red if not
+        lineRenderer.startColor = hasValidTarget ? validColor : invalidColor;
+        lineRenderer.endColor = hasValidTarget ? validColor : invalidColor;
     }
 
     void TryTeleport()
     {
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, 10f, groundLayer))
-        {
-            xrOrigin.position = hit.point;
-        }
+        // Offset the XR origin so the camera stays at head height above the target
+        Vector3 cameraWorldPos = Camera.main.transform.position;
+        Vector3 rigToCamera = new Vector3(
+            cameraWorldPos.x - xrOrigin.position.x,
+            0f,
+            cameraWorldPos.z - xrOrigin.position.z
+        );
+
+        // Place the rig so feet land on the hit point, not the head
+        xrOrigin.position = validHitPoint - rigToCamera;
     }
 }
